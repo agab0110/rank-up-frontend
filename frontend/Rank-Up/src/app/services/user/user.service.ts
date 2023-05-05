107311ec3503b3b073d7f2525943df1a577b70d0
agab0110 <agabrielex01@gmail.com>
2023-05-05 12:26:12 +0200

    removed array error

--- frontend/Rank-Up/src/app/services/user/user.service.ts
+++ frontend/Rank-Up/src/app/services/user/user.service.ts
@@ -2,18 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/classes/user';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
   private userUrl: string;
 
   constructor(private http: HttpClient) {
     this.userUrl = 'http://localhost:8080/api'
   }
-
+  
   public save(user: User) {
-    return this.http.post<User[]>(
+    return this.http.post<User>(
       this.userUrl + "/signup", user
     );
   }
