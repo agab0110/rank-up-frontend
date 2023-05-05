1e554bc741957c531e2860bb5b1f1f297da2e469
agab0110 <agabrielex01@gmail.com>
2023-05-05 12:39:20 +0200

    added signup function and service

--- frontend/Rank-Up/src/app/signup/signup.page.ts
+++ frontend/Rank-Up/src/app/signup/signup.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../classes/user';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
-  
+export class SignupPage {
+  user: User;
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private service: UserService
+    ) {
+    this.user = new User();
   }
 
   signup() {
+    this.service.save(this.user).subscribe(result => this.goToLogin());
+  }
+
+  goToLogin() {
     this.router.navigate(['/login']);
   }
 }
