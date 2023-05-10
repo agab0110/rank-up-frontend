9f0f32bd5a79fb6fba72fa126ad08f9d49cfa87e
Paolo <paolodibiase1@gmail.com>
2023-05-10 16:50:32 +0200

    implemented api to change name and delete a team

--- frontend/Rank-Up/src/app/services/team/team.service.ts
+++ frontend/Rank-Up/src/app/services/team/team.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
+  private teamUrl: string;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+    this.teamUrl = 'http://localhost:8080/teamApi'
+  }
+
+  public changeTeamName(teamId: number, teamName: string) {
+    return this.http.patch(
+      this.teamUrl + "/changeName/" + teamId, teamName
+    );
+  }
+
+  public deleteTeam(teamId: number) {
+    return this.http.delete(
+      this.teamUrl + "/deleteTeam/" + teamId
+    );
+  }
 }
