763769e82626357ebf1a2def5d8da91dec659bba
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:50:15 +0200

    updated class team

--- frontend/Rank-Up/src/app/models/team/team.ts
+++ frontend/Rank-Up/src/app/models/team/team.ts
@@ -1,2 +1,19 @@
+import { Admin } from "../admin/admin";
+import { Prize } from "../prize/prize";
+import { Rule } from "../rule/rule";
+import { UserJoinsTeam } from "../userJoinsTeam/user-joins-team";
+
 export class Team {
+    codice!: Long;
+    name!: string;
+    photo!: string;
+    privacy!: boolean;
+    pointVisibility!: boolean;
+    prizes!: Prize[];
+    notifications!: Notification[];
+    rules!: Rule[];
+    tasks!: Task[];
+    userJoinsTeam!: UserJoinsTeam[];
+    creatorAdmin!: Admin;
+    admins!: Admin[];
 }
