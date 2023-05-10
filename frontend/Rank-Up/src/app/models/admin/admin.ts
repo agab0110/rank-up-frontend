72f97588256441c88b5dc3bc67aaae91fc2fc269
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:11:17 +0200

    updated class admin

--- frontend/Rank-Up/src/app/models/admin/admin.ts
+++ frontend/Rank-Up/src/app/models/admin/admin.ts
@@ -1,2 +1,17 @@
+import { Prize } from "../prize/prize";
+import { Rule } from "../rule/rule";
+import { RuleCompleted } from "../ruleCompleted/rule-completed";
+import { TaskCompleted } from "../taskCompleted/task-completed";
+import { Team } from "../team/team";
+import { User } from "../user/user";
+
 export class Admin {
+    id!: number;
+    userToAdmin!: User;
+    createdTasks!: Task[];
+    createdRules!: Rule[];
+    createdPrizes!: Prize[];
+    createdTeams!: Team[];
+    rulesRevisioned!: RuleCompleted[];
+    taskRevisioned!: TaskCompleted[];
 }
