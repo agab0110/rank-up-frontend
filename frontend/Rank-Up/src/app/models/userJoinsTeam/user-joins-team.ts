cf961cc941c08b1ab6ab35a4fb1e35d26838e6b7
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:02:27 +0200

    updated class user joins team

--- frontend/Rank-Up/src/app/models/userJoinsTeam/user-joins-team.ts
+++ frontend/Rank-Up/src/app/models/userJoinsTeam/user-joins-team.ts
@@ -1,2 +1,11 @@
+import { UserJoinsTeamKey } from "../keys/userJoinsTeamKey/user-joins-team-key";
+import { Team } from "../team/team";
+import { User } from "../user/user";
+
 export class UserJoinsTeam {
+    key!: UserJoinsTeamKey;
+    user!: User;
+    team!: Team;
+    points!: number;
+    accepted!: boolean;
 }
