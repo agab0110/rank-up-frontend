1a1a40330c2f9334ec3c63e46e9cc079a6e9322a
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:50:30 +0200

    updated class user

--- frontend/Rank-Up/src/app/models/user/user.ts
+++ frontend/Rank-Up/src/app/models/user/user.ts
@@ -1,9 +1,23 @@
+import { Admin } from "../admin/admin";
+import { RuleCompleted } from "../ruleCompleted/rule-completed";
+import { Task } from "../task/task";
+import { TaskCompleted } from "../taskCompleted/task-completed";
+import { UserGetPrize } from "../userGetPrize/user-get-prize";
+import { UserJoinsTeam } from "../userJoinsTeam/user-joins-team";
+
 export class User {
-    id!: Number;
+    id!: number;
     name!: string;
     surname!: string;
     email!: string;
     username!: string;
     password!: string;
-
+    photo!: string;
+    adminToUser!: Admin;
+    tasksCompleted!: TaskCompleted[];
+    rulesCompleted!: RuleCompleted[];
+    notifications!: Notification[];
+    assignedTasks!: Task[];
+    userJoinsTeams!: UserJoinsTeam[];
+    userGetPrize!: UserGetPrize[];
 }
