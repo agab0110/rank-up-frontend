af9661538b9986a1431e6d43aed784c1809bbaf6
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:29:58 +0200

    updated class rule completed

--- frontend/Rank-Up/src/app/models/ruleCompleted/rule-completed.ts
+++ frontend/Rank-Up/src/app/models/ruleCompleted/rule-completed.ts
@@ -1,2 +1,17 @@
+import { Timestamp } from "rxjs";
+import { Rule } from "../rule/rule";
+import { Admin } from "../admin/admin";
+import { User } from "../user/user";
+
 export class RuleCompleted {
+    id!: number;
+    attached!: string;
+    timeStamp!: Timestamp<any>;
+    rule!: Rule;
+    admin!: Admin;
+    user!: User;
+    revisionDate!: Date;
+    bonus!: number;
+    comment!: string;
+    acceptance!: boolean;
 }
