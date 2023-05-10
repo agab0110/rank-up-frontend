ad4ea72b8f6335dcec6cbf5d3b4944d3ee84e48d
Paolo <paolodibiase1@gmail.com>
2023-05-10 15:16:43 +0200

    updated class notification

--- frontend/Rank-Up/src/app/models/notification/notification.ts
+++ frontend/Rank-Up/src/app/models/notification/notification.ts
@@ -1,2 +1,11 @@
+import { Team } from "../team/team";
+import { User } from "../user/user";
+
 export class Notification {
+    id!: number;
+    title!: string;
+    description!: string;
+    date!: Date;
+    team!: Team;
+    user!: User;
 }
