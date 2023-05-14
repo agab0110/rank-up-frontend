df25911cbf962e90012d80e6621bbf50da62723d
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-14 13:06:57 +0200

    added link backend

--- frontend/Rank-Up/src/app/services/ruleCompleted/rule-completed.service.ts
+++ frontend/Rank-Up/src/app/services/ruleCompleted/rule-completed.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RuleCompleted } from 'src/app/models/ruleCompleted/rule-completed';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RuleCompletedService {
+  private baseUrl: String;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:8080/ruleCompletedApi";
+   }
+
+   public ruleAccepted(teamId: Number): Observable<RuleCompleted[]> {
+    return this.http.get<RuleCompleted[]>(
+      this.baseUrl + "/ruleAccepted/" + teamId
+    );
+  }
+  public rulerejected(teamId: Number): Observable<RuleCompleted[]> {
+    return this.http.get<RuleCompleted[]>(
+      this.baseUrl + "/rulerejected/" + teamId
+    );
+  }
 }
