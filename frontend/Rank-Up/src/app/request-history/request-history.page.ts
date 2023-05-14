2ebc2449a6c9070b96b47f9e383d2b903ecd0bbe
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-14 13:09:23 +0200

    added function rulecompleted end task completed

--- frontend/Rank-Up/src/app/request-history/request-history.page.ts
+++ frontend/Rank-Up/src/app/request-history/request-history.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Location } from '@angular/common';
+import { TaskCompleted } from '../models/taskCompleted/task-completed';
+import { TaskCompletedService } from '../services/taskCompleted/task-completed.service';
+import { RuleCompletedService } from '../services/ruleCompleted/rule-completed.service';
+import { RuleCompleted } from '../models/ruleCompleted/rule-completed';
 
 @Component({
   selector: 'app-request-history',
@@ -8,10 +12,18 @@ import { Location } from '@angular/common';
   styleUrls: ['./request-history.page.scss'],
 })
 export class RequestHistoryPage implements OnInit {
-
+  rulecompleted : RuleCompleted[];
+  ruleRejected : RuleCompleted[];
+  taskCompleted : TaskCompleted[];
+  taskRejected : TaskCompleted[];
   filter: number = 1;
 
-  constructor(private alertController: AlertController, private location: Location) { }
+  constructor(private alertController: AlertController, private location: Location, private rulecompletedservice : RuleCompletedService, private taskcompletedservice : TaskCompletedService) {
+    this.rulecompleted = new Array<RuleCompleted>;
+    this.ruleRejected = new Array<RuleCompleted>;
+    this.taskCompleted = new Array<TaskCompleted>;
+    this.taskRejected = new Array<TaskCompleted>;
+   }
 
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -49,6 +61,58 @@ export class RequestHistoryPage implements OnInit {
   
   backButton() {
     this.location.back();
+  }
+
+  ruleComleleted(){
+    this.rulecompletedservice.ruleAccepted(1).subscribe(Response =>{
+      this.rulecompleted = Response;
+    },(error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+      });
+  }
+
+  rulerejected(){
+    this.rulecompletedservice.rulerejected(1).subscribe(Response =>{
+      this.ruleRejected = Response;
+    },(error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+      });
+  }
+
+  taskAccepted(){
+    this.taskcompletedservice.taskAccepted(1).subscribe(Response =>{
+      this.taskCompleted = Response;
+    },(error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+      });
+  }
+
+  taskrejected(){
+    this.taskcompletedservice.taskRejected(1).subscribe(Response =>{
+      this.taskRejected = Response;
+    },(error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+      });
   }
 
 }
