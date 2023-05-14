6137350855c97c1835824143d7e5bf6b2e7cd4ca
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-14 12:58:54 +0200

    added function list rule and task

--- frontend/Rank-Up/src/app/team-rules-tasks/team-rules-tasks.page.ts
+++ frontend/Rank-Up/src/app/team-rules-tasks/team-rules-tasks.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Rule } from '../models/rule/rule';
+import { RuleService } from '../services/rule/rule.service';
+import { TaskService } from '../services/task/task.service';
+import { Task } from '../models/task/task';
 
 @Component({
   selector: 'app-team-rules-tasks',
@@ -11,11 +15,19 @@ export class TeamRulesTasksPage implements OnInit {
   public alertBtns = ["Accetta", "Rifiuta"];
   type='rules';
   stato= false;
-
-  constructor(private location: Location) { }
+  rules : Rule[];
+  tasks : Task[];
+  constructor(private location: Location, private ruleservice : RuleService, private taskservice :TaskService) {
+    this.rules = new Array<Rule>;
+    this.tasks = new Array<Task>;
+    
+   }
 
   ngOnInit() {
+    this.ListRule();
+    this.ListTask();
   }
+
 
   backButton() {
     this.location.back();
@@ -23,5 +35,29 @@ export class TeamRulesTasksPage implements OnInit {
   segmentChanged(event: any) {
     this.stato = !this.stato;
   }
+  ListRule(){
+    this.ruleservice.listRule(1).subscribe(response =>{
+      this.rules = response;
+    }, (error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+    });
+  }
 
+  ListTask(){
+    this.taskservice.listTask(2).subscribe(response =>{
+      this.tasks = response;
+    }, (error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+    });
+  }
 }
