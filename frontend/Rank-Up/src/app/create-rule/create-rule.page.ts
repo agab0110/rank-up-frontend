c18a0a0b2a5c944bf35962daf006f0a3886443de
MikeClngl <michele.colangelo02@gmail.com>
2023-05-14 12:08:03 +0200

    POST api to frontend for rule

--- frontend/Rank-Up/src/app/create-rule/create-rule.page.ts
+++ frontend/Rank-Up/src/app/create-rule/create-rule.page.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { RuleService } from '../services/rule/rule.service';
+import { Rule } from '../models/rule/rule';
+import { Team } from '../models/team/team';
+import { Admin } from '../models/admin/admin';
 
 @Component({
   selector: 'app-create-rule',
   templateUrl: './create-rule.page.html',
   styleUrls: ['./create-rule.page.scss'],
 })
-export class CreateRulePage implements OnInit {
+export class CreateRulePage implements OnInit{
+  rule: Rule;
+  team: Team;
+  admin: Admin;
 
-  constructor(private location: Location) { }
+  constructor(
+    private location: Location,
+    private ruleService:  RuleService
+    ) {
+    this.rule = new Rule();
+    this.team = new Team();
+    this.admin = new Admin();
+  }
 
-  ngOnInit() {
+  ngOnInit(){
+    if(localStorage.getItem('team') == null || localStorage.getItem('team') == '')
+    //this.router.navigate(['user/home']);
+    this.team = JSON.parse(localStorage.getItem('team') || '{}');
+    if(localStorage.getItem('admin') == null || localStorage.getItem('admin') == '')
+    //this.router.navigate(['user/home']);
+    this.admin = JSON.parse(localStorage.getItem('admin') || '{}');
   }
 
   backButton() {
     this.location.back();
   }
 
+  public createRule(){
+      //this.rule.admin = this.admin;
+      //this.rule.team = this.team;
+      this.ruleService.newRule(this.rule).subscribe(response => {
+      console.log("Regola creata con successo");
+      console.log(response);
+    }, (error: Response) => {
+      if (error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+    });
+  }
 }
