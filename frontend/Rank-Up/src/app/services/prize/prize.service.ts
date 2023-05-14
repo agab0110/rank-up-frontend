c28d963c3a9ee75fb7e5eb2f1bb6bec67fed260e
MikeClngl <michele.colangelo02@gmail.com>
2023-05-14 12:05:22 +0200

    POST api to frontend for prize

--- frontend/Rank-Up/src/app/services/prize/prize.service.ts
+++ frontend/Rank-Up/src/app/services/prize/prize.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Prize } from 'src/app/models/prize/prize';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrizeService {
+  private baseUrl : String;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:8080/prizeApi";
+   }
+
+   public newPrize(prize: Prize) {
+    return this.http.post<Prize>(
+    this.baseUrl + "/createPrize", prize);
+   }
 }
