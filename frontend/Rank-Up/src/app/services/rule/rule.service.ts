6e6845a87ccd94ba6ae642c0d6a4f6798453b073
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-14 13:00:47 +0200

    added link backend

--- frontend/Rank-Up/src/app/services/rule/rule.service.ts
+++ frontend/Rank-Up/src/app/services/rule/rule.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rule } from 'src/app/models/rule/rule';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RuleService {
+  private baseUrl: String;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:8080/ruleApi";
+  }
+
+  public listRule(teamId: Number): Observable<Rule[]> {
+    return this.http.get<Rule[]>(
+      this.baseUrl + "/rules/" + teamId
+    );
+  }
 }
