eee47d0bc40c63eae6df9345ab089600ea0633c1
agab0110 <agabrielex01@gmail.com>
2023-05-19 12:45:11 +0200

    added partecipants and partecipantsPoint

--- frontend/Rank-Up/src/app/services/userJoinsTeam/user-joins-team.service.ts
+++ frontend/Rank-Up/src/app/services/userJoinsTeam/user-joins-team.service.ts
@@ -8,17 +8,15 @@ import { UserJoinsTeam } from 'src/app/models/userJoinsTeam/user-joins-team';
   providedIn: 'root'
 })
 export class UserJoinsTeamService {
-  private accessRequestUrl: string;
   status!: string;
   userJoinsTeamUrl: string;
 
   constructor(private http: HttpClient) {
-    this.accessRequestUrl = 'http://localhost:8080/accessRequestApi',
     this.userJoinsTeamUrl = 'http://localhost:8080/userJoinsTeamApi';
    }
 
    deleteRequest(userJoinTeam: UserJoinsTeam) {
-    return this.http.delete(this.accessRequestUrl + "/deleteRequest")
+    return this.http.delete(this.userJoinsTeamUrl + "/deleteRequest")
     .subscribe(() => this.status = 'Delete successful');
   }
 
@@ -29,5 +27,15 @@ export class UserJoinsTeamService {
 
   public getListPendingRequests(id_team: string): Observable<Notification[]> {
     const params = new HttpParams().set('id_team', id_team);
-    return this.http.get<Notification[]>(this.accessRequestUrl + "/list/pendingRequests", {params})};
+    return this.http.get<Notification[]>(this.userJoinsTeamUrl + "/list/pendingRequests", {params})
+  }
+
+  public getPartecipants(idTeam: number): Observable<User[]> {
+    return this.http.get<User[]>(this.userJoinsTeamUrl + "/partecipants/" + idTeam);
+  }
+
+  public getPartecipantsPoints(idTeam: number): Observable<UserJoinsTeam[]> {
+    return this.http.get<UserJoinsTeam[]>(this.userJoinsTeamUrl + "/partecipantsPoints/" + idTeam);
+  }
+
 }
