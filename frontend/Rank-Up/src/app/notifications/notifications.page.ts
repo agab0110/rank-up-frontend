dfa7fa1d0fc51ba784a0d2171dcc4fde88760588
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-24 22:06:34 +0200

    added api n 64

--- frontend/Rank-Up/src/app/notifications/notifications.page.ts
+++ frontend/Rank-Up/src/app/notifications/notifications.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { NotificationService } from '../services/notification/notification.service';
+import { User } from '../models/user/user';
+import { Team } from '../models/team/team';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
@@ -9,10 +12,25 @@ export class NotificationsPage implements OnInit {
 
   public alertBtns = ["Accetta", "Rifiuta"];
   type='utente';
+  notifications:Notification[];
+  user: User;
+  team: Team;
+  notification!:Notification;
+  constructor(private location: Location, private notificationService:NotificationService) { 
+    this.notifications = new Array<Notification>;
+    this.team = new Team();
+    this.user = new User();
 
-  constructor(private location: Location) { }
+  }
 
   ngOnInit() {
+    //if(localStorage.getItem('team') == null || localStorage.getItem('team') == '')
+    //this.router.navigate(['user/home']);
+    //this.team = JSON.parse(localStorage.getItem('team') || '{}');
+    //if(localStorage.getItem('admin') == null || localStorage.getItem('admin') == '')
+    //this.router.navigate(['user/home']);
+    //this.admin = JSON.parse(localStorage.getItem('admin') || '{}');
+    this.getNotification();
   }
 
   segmentChanged(ev: any){
@@ -21,5 +39,17 @@ export class NotificationsPage implements OnInit {
   
   backButton() {
     this.location.back();
+  }
+  getNotification(){
+    this.notificationService.getNotification(1).subscribe(response =>{
+      this.notifications = response;
+    }, (error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+    });
   }
 }
--- frontend/Rank-Up/src/app/services/notification/notification.service.ts
+++ frontend/Rank-Up/src/app/services/notification/notification.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  private notificationUrl:  String;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.notificationUrl = "http://localhost:8080/notificationApi";
+  }
+
+  public getNotification(idUser: Number): Observable<Notification[]> {
+    return this.http.get<Notification[]>(
+      this.notificationUrl + "/getNotification/" + idUser
+    );
+  }
+
 }
