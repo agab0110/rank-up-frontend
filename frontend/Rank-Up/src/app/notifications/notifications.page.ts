a1eecbc29b912d6542ccf63ea452b3357c2ee012
MikeClngl <michele.colangelo02@gmail.com>
2023-05-24 19:32:17 +0200

    add api for user notification

--- frontend/Rank-Up/src/app/notifications/notifications.page.ts
+++ frontend/Rank-Up/src/app/notifications/notifications.page.ts
@@ -1,25 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+
+import { NotificationService } from '../services/notification/notification.service';
+import { Notification } from '../models/notification/notification';
+import { User } from '../models/user/user';
+
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
+  userNotification : Notification[];
+  user : User;
+  stato : Boolean;
 
   public alertBtns = ["Accetta", "Rifiuta"];
-  type='utente';
+  type='admin';
 
-  constructor(private location: Location) { }
+  constructor(private location: Location, private notificationService: NotificationService) {
+      this.userNotification = new Array<Notification>;
+      this.user = new User;
+      this.stato = true;
+    }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
   }
 
-  segmentChanged(ev: any){
-    console.log('Type changed ', ev)
+  segmentChanged(ev: any, idUser: Number){
+    this.stato = !this.stato;
+    if(!this.stato){
+      this.getUserNotification(idUser);
+    }else{
+      this.getAdminNotification()
+    }
   }
-  
+
   backButton() {
     this.location.back();
   }
+
+  public getUserNotification(idUser: Number) {
+    this.notificationService.getUserNotifications(idUser).subscribe(response => {
+      this.userNotification = response;
+    }, (error: Response) => {
+      if(error.status == 400)
+        console.log("400 error");
+      else {
+        console.log('An unexpected error occured');
+      }
+      console.log(error);
+    });
+  }
+
+  public getAdminNotification(){}
 }
