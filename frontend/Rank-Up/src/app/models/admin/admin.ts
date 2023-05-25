7e7ae1b205b159ac397a2d968e350b9dc144ff01
agab0110 <agabrielex01@gmail.com>
2023-05-25 11:28:56 +0200

    added notification

--- frontend/Rank-Up/src/app/models/admin/admin.ts
+++ frontend/Rank-Up/src/app/models/admin/admin.ts
@@ -1,7 +1,4 @@
-import { Prize } from "../prize/prize";
-import { Rule } from "../rule/rule";
-import { RuleCompleted } from "../ruleCompleted/rule-completed";
-import { TaskCompleted } from "../taskCompleted/task-completed";
+import { AdminReciveNotification } from "../adminReciveNotification/admin-recive-notification";
 import { Team } from "../team/team";
 import { User } from "../user/user";
 
@@ -9,4 +6,5 @@ export class Admin {
     id!: number;
     user!: User;
     team!: Team;
+    notifications!: AdminReciveNotification[];
 }
