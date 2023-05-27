3ab35913caabc380fc0e385ffc370382d0e38b71
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-27 11:52:40 +0200

    added local storage

--- frontend/Rank-Up/src/app/prizes-page/prizes-page.page.ts
+++ frontend/Rank-Up/src/app/prizes-page/prizes-page.page.ts
@@ -6,6 +6,7 @@ import { Prize } from '../models/prize/prize';
 import { User } from '../models/user/user';
 import { Team } from '../models/team/team';
 import { Router } from '@angular/router';
+import { UserJoinsTeam } from '../models/userJoinsTeam/user-joins-team';
 
 @Component({
   selector: 'app-prizes-page',
@@ -16,19 +17,18 @@ export class PrizesPagePage implements OnInit {
   prizes: Prize[];
   user: User;
   team: Team;
+  userjoin:UserJoinsTeam;
 
   constructor(
     public alertCtrl: AlertController,
     private prizeService: PrizeService,
     private router: Router
     ) {
-      this.prizes = new Array<Prize>
-      this.user = new User
-      this.team = new Team
+      this.prizes = new Array<Prize>;
+      this.user = new User();
+      this.team = new Team();
+      this.userjoin = new UserJoinsTeam();
      }
-
-  user_name: string = '[Nome Utente]';
-  user_points: number = 500
 
 
   async showAlert(name: string, points: number) {
@@ -44,7 +44,7 @@ export class PrizesPagePage implements OnInit {
           text: 'Si',
           cssClass:'alert-button-blue',
           handler: () => {
-           this.user_points -= points;
+           this.userjoin.points -= points;
           }
         }
       ]
@@ -56,6 +56,7 @@ export class PrizesPagePage implements OnInit {
   ngOnInit() {
     this.team = JSON.parse(localStorage.getItem('team') || '{}');
     this.user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.userjoin =  JSON.parse(localStorage.getItem('viewUserJoinsTeam') || '{}');
     if (localStorage.getItem('team') == null) {
       this.router.navigate(["/user/home"]);
     }
