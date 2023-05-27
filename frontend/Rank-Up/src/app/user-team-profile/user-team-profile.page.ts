761cbe36aac89777f5c66b7ad7f33ab0db250027
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-27 10:30:28 +0200

    added local storage

--- frontend/Rank-Up/src/app/user-team-profile/user-team-profile.page.ts
+++ frontend/Rank-Up/src/app/user-team-profile/user-team-profile.page.ts
@@ -5,6 +5,8 @@ import { Team } from '../models/team/team';
 import { TaskCompletedService } from '../services/taskCompleted/task-completed.service';
 import { RuleCompletedService } from '../services/ruleCompleted/rule-completed.service';
 import { UserGetPrizeService } from '../services/userGetPrize/user-get-prize.service';
+import { Router } from '@angular/router';
+import { UserJoinsTeam } from '../models/userJoinsTeam/user-joins-team';
 
 @Component({
   selector: 'app-user-team-profile',
@@ -19,37 +21,48 @@ export class UserTeamProfilePage implements OnInit {
 
   user: User;
   team: Team;
+  userjoin:UserJoinsTeam;
 
   constructor(
     private location: Location,
     private taskCompletedService: TaskCompletedService,
     private ruleCompletedService: RuleCompletedService,
-    private userGetPrizeService: UserGetPrizeService
+    private userGetPrizeService: UserGetPrizeService,
+    private router: Router
     ) {
     this.user = new User();
     this.team = new Team();
+    this.userjoin = new UserJoinsTeam();
     this.activities = [];
     this.prizes = [];
    }
 
   ngOnInit() {
-    //TODO: inserire local storage quando sarà pronto
+    this.team = JSON.parse(localStorage.getItem('team') || '{}');
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.userjoin =  JSON.parse(localStorage.getItem('viewUserJoinsTeam') || '{}');
+    if (localStorage.getItem('team') == null) {
+      this.router.navigate(["/user/home"]);
+    }
+    if (localStorage.getItem('user') == null) {
+      this.router.navigate(["/login"]);
+    };
 
-    this.taskCompletedService.getTaskCompletedByUser(1,1).subscribe((response) => {
+    this.taskCompletedService.getTaskCompletedByUser(this.user.id,this.team.codice).subscribe((response) => {
       response.forEach(element => {
         this.activities.push(element);
       });
       console.log(response);
     });
 
-    this.ruleCompletedService.getRulesCompletedByUser(1,1).subscribe((response) => {
+    this.ruleCompletedService.getRulesCompletedByUser(this.user.id,this.team.codice).subscribe((response) => {
       response.forEach(element => {
         this.activities.push(element);
       });
       console.log(response);
     });
 
-    this.userGetPrizeService.getUserPrizes(1,1).subscribe((response) => {
+    this.userGetPrizeService.getUserPrizes(this.user.id,this.team.codice).subscribe((response) => {
       response.forEach(element => {
         this.prizes.push(element);
       });
